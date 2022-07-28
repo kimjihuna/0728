e2464a4418e5fdb4bf8cd3fd74815ddbca576713
User <User@DESKTOP-3F8JG65>
2022-07-28 18:38:09 +0900

    수정

--- src/Problum.java
+++ src/Problum.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Problum {
 	public static void main(String[] args) {
-		//문제 1
+		// 문제 1
 		int count = 0;
 		int row = 0;
 		for (int i = 65; i < 91; i++) {
@@ -26,11 +26,28 @@ public class Problum {
 		// 문제 2 bmi
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("키를 입력하세요 : ");
+		System.out.print("키를 입력하세요 : ");
 		double height = sc.nextDouble();
+		double height2 = height / 100;
+		System.out.print("몸무게를 입력하세요 : ");
+		double weight = sc.nextInt();
 
-		System.out.println("몸무게를 입력하세요 : ");
-		int weight = sc.nextInt();
+		double BMI = weight / Math.pow(height2, 2);
+//		System.out.println("BMI : "+BMI);
+
+		if (BMI >= 35) {
+			System.out.println("고도비만");
+		} else if (BMI <= 34.9 && BMI >= 30) {
+			System.out.println("중도비만");
+		} else if (BMI <= 29.9 && BMI >= 25) {
+			System.out.println("경도비만");
+		} else if (BMI <= 24.9 && BMI >= 23) {
+			System.out.println("과체중");
+		} else if (BMI <= 22.9 && BMI >= 18.5) {
+			System.out.println("정상");
+		} else if (BMI < 18.5) {
+			System.out.println("중도비만");
+		}
 
 	}
 
